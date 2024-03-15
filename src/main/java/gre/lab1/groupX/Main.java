de9c9d268b01c07b2d54d4381bc0e2a259bd9bac
borishuu <boris.hutzli@heig-vd.ch>
2024-03-15 13:33:44 +0100

    initial commit

---
+++ src/main/java/gre/lab1/groupX/Main.java
@@ -0,0 +1,18 @@
+package gre.lab1.groupX;
+
+import java.io.IOException;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    // Lecture d'un graphe orienté depuis un fichier
+    // DirectedGraph graph = DirectedGraphReader.fromFile("data/file.txt");
+
+    // TODO
+    //  - Renommage du package ;
+    //  - Écrire le code dans les fichiers Main.java, TarjanAlgorithm.java, ContractionAlgorithm.java
+    //    et UNIQUEMENT ceux-ci ;
+    //  - Documentation soignée comprenant :
+    //    - la javadoc, avec auteurs et description des implémentations ;
+    //    - des commentaires sur les différentes parties de vos algorithmes.
+  }
+}
